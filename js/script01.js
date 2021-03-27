73f0f88f4e0d02cc664864208108e061409dbc66
Vinícius Carvalho Ribeiro Quirino <cabohdmiusb@hotmail.com>
2021-03-26 23:09:43 -0300

    the end is near
    The project is almost in his end. Only a few details are missing

---
+++ js/script01.js
@@ -0,0 +1,38 @@
+let loginbutton = document.getElementById('login-btn');
+
+let textlogin = document.getElementById('login-i');
+let textpasswd = document.getElementById('login-p');
+
+function clickar(){
+
+
+    if ( textlogin.value == 'Syniila' && textpasswd.value == '123' ){
+        window.open( "../index/index_adm.html" , "_top")
+    } else if ( textlogin.value == 'Vinicius' && textpasswd.value == '456'){   
+        window.open( "../index/index_cliente.html" , "_top")
+    } else if ( textlogin.value != 'Syniila' && textlogin.value.length != 0 || textpasswd.value != '123' && textpasswd.value.length != 0 ){
+        window.alert('Este usuário não está cadastrado! Verifique o login e a senha e tente novamente') 
+    } else{
+        window.alert('Os campos estão vazios! Preencha-os para poder prosseguir')
+    }
+ 
+}
+
+document.addEventListener("keydown", function(event){
+    
+    if (event.key === 'Enter'){
+        let textlogin = document.getElementById('login-i');
+        let textpasswd = document.getElementById('login-p');
+        if ( textlogin.value == 'Syniila' && textpasswd.value == '123' ){
+            window.open( "../index/index_adm.html" , "_top")
+        } else if ( textlogin.value == 'Vinicius' && textpasswd.value == '456'){   
+            window.open( "../index/index_cliente.html" , "_top")
+        } else if ( textlogin.value != 'Syniila' && textlogin.value.length != 0 || textpasswd.value != '123' && textpasswd.value.length != 0 ){
+            window.alert('Este usuário não está cadastrado! Verifique o login e a senha e tente novamente') 
+        } else {
+            window.alert('Os campos estão vazios! Preencha-os para poder prosseguir')
+        }
+}
+ 
+});
+
